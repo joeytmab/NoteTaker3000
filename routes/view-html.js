bfb92a7ebe86bdf060bdd8756e0d071fc7b54fa8
Joey Binas <joseph.t.binas@gmail.com>
2022-06-08 03:44:10 -0700

    removed listener

--- routes/view-html.js
+++ routes/view-html.js
@@ -2,6 +2,7 @@ const path = require('path');
 const router = require('express').Router();
 
 
+
 //GET routes
 
 router.get('/', (req, res) => {
@@ -12,8 +13,5 @@ router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-app.listen(PORT, () => {
-    console.log(`Application active and listening on localhost:${PORT}.`)
-});
 
 module.exports = router;
